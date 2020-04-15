a40f23c434a3f980e676b175e498b4b1e1d88766
Josh Castaneda <josh.a.castaneda@gmail.com>
2020-04-14 21:37:56 -0700

    update requirements.txt and README
    for happy/golden path setup and a clean/fresh install in an effor to be
    more beginner friendly.

--- win_run.bat
+++ win_run.bat
@@ -5,6 +5,8 @@ set FLASK_DEBUG=1
 start redis/redis-server.exe
 start cmd /k python RLBotServer.py
 start cmd /k celery -A backend.tasks.celery_worker.celery worker --pool=solo -l info
+:: TODO: find out if this still works. On windows, in gitbash, i had to run flower with celery like below
+:: celery flower -A backend.tasks.celery_worker.celery --port=5555
 start flower --port=5555
 cd webapp
 npm start
